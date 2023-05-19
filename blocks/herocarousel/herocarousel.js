5882996816cba1fd24e0518a2918b512fefb65bb
Sakshi Arora <sakarora@Sakshis-MacBook-Pro.local>
2023-05-19 10:32:09 +0530

    Added accessibility to hero carousel.

--- blocks/herocarousel/herocarousel.js
+++ blocks/herocarousel/herocarousel.js
@@ -9,8 +9,28 @@ export default function decorate(block) {
         listItem.append(anchorLink);
         carouselList.append(listItem);
     };
+    const handleCarouselItemFocus = function (carouselItem) {
+        const headingElement = carouselItem.querySelector('h2');
+        const carouselList = block.parentElement.querySelector(".herocarousel_nav");
+        const anchorLink = carouselList.querySelector('[href="#' + headingElement.id + '"]');
+        if (block.scrollLeft == carouselItem.offsetLeft) {
+            carouselItem.setAttribute("aria-selected", true);
+            anchorLink.setAttribute("aria-selected", true);
+            anchorLink.classList.add("active");
+        } else {
+            carouselItem.setAttribute("aria-selected", false);
+            anchorLink.setAttribute("aria-selected", false);
+            anchorLink.classList.remove("active");
+        }
+    };
+    const handleScroll = function (event) {
+        carouselItems.forEach(handleCarouselItemFocus);
+    };
 
     carouselItems.forEach(createCarouselListItem);
     carouselList.classList.add("herocarousel_nav");
     block.insertAdjacentElement("afterend", carouselList);
+    block.onscroll = handleScroll;
+    //for setting attributes on initial load
+    handleCarouselItemFocus(carouselItems[0]);
 }
